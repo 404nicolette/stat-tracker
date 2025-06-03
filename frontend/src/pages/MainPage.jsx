a67d58a8b680aae9ffae732227517a349145cd06
404nicolette <123152389+404nicolette@users.noreply.github.com>
2025-06-03 01:38:08 +0100

    feat: ladning page done and is responsive

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -5,7 +5,7 @@ import Nav from '../components/Nav'
 const MainPage = () => {
   return (
     <div className='border-2 border-red-700 w-full h-screen bg-[#e4e4e4] flex flex-col items-center justify-center'>
-        <Nav/>
+        <Nav/>       
         <VolleyballCourt/>
     </div>
   )
